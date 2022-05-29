a6777a885b1b0676ff1b40d8d52a4f19b8219fe3
Mustard <calva_cartablanca@hotmail.com>
2022-05-28 21:20:04 -0500

    Actualizacion Gus

--- Assets/Scripts/UI/Inventario/Inventory_Player.cs
+++ Assets/Scripts/UI/Inventario/Inventory_Player.cs
@@ -9,6 +9,7 @@ public enum TipoItems
     PocionStamina,
     PocionFuego,
     PocionHielo,
+    Moneda,
     PocionCuracion
 }
 
@@ -30,6 +31,16 @@ public class Inventory_Player : MonoBehaviour
 
     public static Inventory_Player estancia;
     public Itemclass[] items;
+
+    public void vaciarSlot(int numslot,Image img)
+    {
+        items[numslot].lleno = false;
+        items[numslot].cantidad = 0;
+        items[numslot].textslot.text = items[numslot].cantidad.ToString();
+        items[numslot].type = TipoItems.Vacio;
+        img.sprite = null;
+        img.enabled = false;
+    }
 
     private void Awake()
     {
